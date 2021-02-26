5e2faded9f9ee2286efa1fbca5d523d0c395a9a7
Vladislav <infohunterr@gmail.com>
2021-02-26 17:43:33 +0600

    modal window

--- js/dist/index.dev.js
+++ js/dist/index.dev.js
@@ -51,4 +51,47 @@ function workAnimate() {
 openMail();
 buttonAnimate();
 workAnimate();
-new WOW().init();
+new WOW().init(); // Модальное окно
+
+var openModal = function openModal(triggerSelector, modalDataSelector) {
+  // объявляем функцию открытия модального окна, которая принимает в качестве параметров селекторы кнопки и соответствующего модального окна
+  var trigger = document.querySelector(triggerSelector); // ищем кнопку по переданному селектору
+
+  var modal = document.querySelector(modalDataSelector); // ищем модальное окно по переданному селектору
+
+  if (!trigger || !modal) return; // если такая кнопка или модальное окно не найдены, то прекращаем работу функции
+
+  trigger.addEventListener("click", function (e) {
+    // при клике на кнопку
+    e.preventDefault(); // предотвращаем браузерные действия (если кнопка сделана через тег ссылки <a href=""></a>, то отменяется переход по ссылке)
+
+    modal.classList.add("modal_active"); // отображаем модальное окно, добавив ему активный класс
+  });
+};
+
+openModal(".no_work", '.modal[data-modal="one"]'); // Запускаем функцию и передаем селекторы для первого модального окна
+
+var closeModal = function closeModal() {
+  // объявляем функцию закрытия модального окна
+  var modals = document.querySelectorAll(".modal"); // ищем все модальные окна
+
+  if (!modals) return; // если их нет, то прекращаем выполнение функции
+
+  modals.forEach(function (el) {
+    // если есть, то для каждого из них
+    el.addEventListener("click", function (e) {
+      // при клике
+      if (e.target.closest(".modal__close")) {
+        // если клик был клик на кнопке закрытия
+        el.classList.remove("modal_active"); // то скрываем модальное окно, удаляя активный класс
+      }
+
+      if (!e.target.closest(".modal__body")) {
+        // если клик был за пределами контентной части модального окна, то есть на затемненную область
+        el.classList.remove("modal_active"); // то тоже скрываем модальное окно, удаляя активный класс
+      }
+    });
+  });
+};
+
+closeModal(); // вызываем функцию закрытия
--- js/index.js
+++ js/index.js
@@ -48,3 +48,39 @@ openMail();
 buttonAnimate();
 workAnimate();
 new WOW().init();
+
+// Модальное окно
+
+const openModal = (triggerSelector, modalDataSelector) => {
+  // объявляем функцию открытия модального окна, которая принимает в качестве параметров селекторы кнопки и соответствующего модального окна
+  const trigger = document.querySelector(triggerSelector); // ищем кнопку по переданному селектору
+  const modal = document.querySelector(modalDataSelector); // ищем модальное окно по переданному селектору
+  if (!trigger || !modal) return; // если такая кнопка или модальное окно не найдены, то прекращаем работу функции
+  trigger.addEventListener("click", (e) => {
+    // при клике на кнопку
+    e.preventDefault(); // предотвращаем браузерные действия (если кнопка сделана через тег ссылки <a href=""></a>, то отменяется переход по ссылке)
+    modal.classList.add("modal_active"); // отображаем модальное окно, добавив ему активный класс
+  });
+};
+openModal(".no_work", '.modal[data-modal="one"]'); // Запускаем функцию и передаем селекторы для первого модального окна
+
+const closeModal = () => {
+  // объявляем функцию закрытия модального окна
+  const modals = document.querySelectorAll(".modal"); // ищем все модальные окна
+  if (!modals) return; // если их нет, то прекращаем выполнение функции
+  modals.forEach((el) => {
+    // если есть, то для каждого из них
+    el.addEventListener("click", (e) => {
+      // при клике
+      if (e.target.closest(".modal__close")) {
+        // если клик был клик на кнопке закрытия
+        el.classList.remove("modal_active"); // то скрываем модальное окно, удаляя активный класс
+      }
+      if (!e.target.closest(".modal__body")) {
+        // если клик был за пределами контентной части модального окна, то есть на затемненную область
+        el.classList.remove("modal_active"); // то тоже скрываем модальное окно, удаляя активный класс
+      }
+    });
+  });
+};
+closeModal(); // вызываем функцию закрытия
